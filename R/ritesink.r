21f1a29879f74b6c5ae752b920c538dcdf065a67
leeper <thosjleeper@gmail.com>
2013-08-09 23:18:14 +0200

    expanding ritesink (Issue#26)

--- R/ritesink.r
+++ R/ritesink.r
@@ -1,21 +1,38 @@
-ritesink <- function(new=TRUE){
-	#textcon <- textConnection("thesink","w+")
-	#z <- scan(what="character", n=1, quiet=TRUE)
-	z <- readLines(n=1)
-	#writeLines(z,textcon)
-	if(z=="EXIT"){
-		#close(textcon)
+ritesink <- function(...,evalenv=.GlobalEnv){
+	#if(newsink)
+		# trigger ctext output window on first load
+	z <- readline(prompt="R> ")
+	if(z=="EXIT")
 		invisible()
-	}
 	else{
-		#close(textcon)
-		#source(textcon)
-		out <- try(withVisible(eval(parse(text=z),envir=.GlobalEnv)))
+		out <- try(withVisible(eval(parse(text=z),envir=evalenv)),silent=TRUE)
 		if(!inherits(out,"try-error")){
 			if(out$visible && !is.null(out$value))
 				print(out$value)
 		}
-		ritesink(new=FALSE)
+		else if(grepl("unexpected end of input",out)){
+			complete <- FALSE
+			while(!complete){
+				z <- paste(z, readline(prompt=" + "),sep="")
+				out <- try(withVisible(eval(parse(text=z),envir=evalenv)),silent=TRUE)
+				if(inherits(out,"try-error")){
+					if(!grepl("unexpected end of input",out))
+						complete <- TRUE
+					else
+						cat(out) # convert to tk output
+				}
+				else{
+					complete <- TRUE
+					if(out$visible && !is.null(out$value))
+						print(out$value) # convert to tk output
+				}
+			}
+		}
+		else if(inherits(out,"try-error"))
+			cat(out) # convert to tk output
+		else
+			cat(out$value) # convert to tk output
+		ritesink(newsink=FALSE)
 	}
 }
 
